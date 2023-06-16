bc8b2eb8fe62404cde6e0ea3ea15b29151f67ff3
Tim019283 <136791232+Tim019283@users.noreply.github.com>
2023-06-16 10:17:11 +0200

    script.js

--- script.js
+++ script.js
@@ -7,31 +7,38 @@ document.addEventListener("DOMContentLoaded", function() {
   const highscoreOutput = document.querySelector("#highscoreOutput");
   const allTimeHighscoreValue = document.querySelector("#allTimeHighscoreValue");
   const resetButton = document.querySelector("#resetButton");
-
+  
+// Zet de standaard woorden op nul
   let highscore = 0;
   let allTimeHighscore = 0;
   let rounds = 0;
 
+  // Eventlistener voor de keuze steen
   document.querySelector("#rock").addEventListener("click", function() {
     playGame("steen");
   });
 
+  // Eventlistener voor de keuze papier
   document.querySelector("#paper").addEventListener("click", function() {
     playGame("papier");
   });
 
+  // Eventlistener voor de keuze schaar
   document.querySelector("#scissors").addEventListener("click", function() {
     playGame("schaar");
   });
 
+  // Eventlistener voor de resetknop
   resetButton.addEventListener("click", function() {
     resetGame();
   });
 
+  // Eventlistener voor het afspelen van resetgeluid bij het hoveren over de resetknop
   resetButton.addEventListener("mouseenter", function() {
     playResetSound();
   });
 
+  // Functie om het spel te spelen
   function playGame(humanChoice) {
     humanOutput.textContent = `${humanChoice}`;
     humanOutput.classList.add("choice");
@@ -45,27 +52,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     resultOutput.classList.remove("win", "loss");
 
+    // Geeft het resultaat weer
     if (result === "Jij wint!") {
       resultOutput.classList.add("win");
     } else if (result === "Computer wint!") {
       resultOutput.classList.add("loss");
     }
-
-    updateScore(result);
-    updateRounds();
-
-    setTimeout(function() {
-      humanOutput.classList.remove("choice");
-      computerOutput.classList.remove("choice");
-    }, 1000);
   }
 
+  // Functie om een willekeurige keuze voor de computer te genereren
   function getRandomChoice() {
     const choices = ["steen", "papier", "schaar"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }
 
+  // Functie om te bepalen wie er wint
   function determineWinner(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
       return "Gelijkspel! Speel opnieuw.";
@@ -80,6 +82,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
+  // Functie om de score bij te werken
   function updateScore(result) {
     if (result === "Jij wint!") {
       highscore++;
@@ -94,11 +97,13 @@ document.addEventListener("DOMContentLoaded", function() {
     allTimeHighscoreValue.textContent = allTimeHighscore;
   }
 
+  // Functie om het aantal rondes bij te werken
   function updateRounds() {
     rounds++;
     roundsOutput.textContent = `Rondes: ${rounds}`;
   }
 
+  // Functie om het spel te resetten
   function resetGame() {
     highscore = 0;
     rounds = 0;
@@ -113,8 +118,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //reset geluidje
 var resetButton = document.getElementById("resetButton");
-      var resetSound = document.getElementById("resetSound");
+var resetSound = document.getElementById("resetSound");
 
-      resetButton.addEventListener("mouseover", function() {
-          resetSound.play();
-      });
+resetButton.addEventListener("mouseover", function() {
+  resetSound.play();
+});
